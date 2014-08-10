7a0c15906d37a3e553a2261bbf4f1ef31fc497a6
rowannorth <rowanowlnorth@gmail.com>
2014-08-09 23:57:43 -0700

    Test drive of methods.

--- ClassRoster/ClassRoster/ViewController.swift
+++ ClassRoster/ClassRoster/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        var rowan = Person(firstName: "Rowan", lastName: "North")
+        var sara = Person(firstName: "Sara", lastName: "Wong")
+        
+        var firstNames = ["Kori", "Rowan"]
+        
+        var rosterList = [rowan, sara]
+        
+        //var Ko
+        
+        
+        
+        println(rowan.fullName())
+        
+        println(rosterList[0].firstName + " " + rosterList[0].lastName)
+        
     }
 
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
 
