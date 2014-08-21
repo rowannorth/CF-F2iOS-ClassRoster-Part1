ccf398dececbbd5141b60f325552899e933eb213
rowannorth <rowanowlnorth@gmail.com>
2014-08-20 19:53:52 -0700

    Added ImagePickerController stuff.

--- ClassRoster/ClassRoster/DetailViewController.swift
+++ ClassRoster/ClassRoster/DetailViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UITextFieldDelegate {
+class DetailViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var peopleDestination: Person?
+    
+    var firstLoad = true
    
+    @IBOutlet weak var imageView: UIImageView!
+    
     @IBOutlet weak var firstNameTextField: UITextField!
     
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -19,6 +23,19 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.firstNameTextField.delegate = self
+        self.lastNameTextField.delegate = self
+        
+        if self.peopleDestination!.image != nil {
+            
+            self.imageView.image = self.peopleDestination!.image
+            
+        } else {
+            
+            self.imageView!.image == UIImage (named: "empty-contact-icon")
+            
+        }
         
     }
     
@@ -34,11 +51,61 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
         self.peopleDestination!.firstName = self.firstNameTextField.text
         self.peopleDestination!.lastName = self.lastNameTextField.text
         
+        if self.firstLoad == true {
+            
+            self.firstLoad = false
+            
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        
+        self.view .endEditing(true)
+    }
+    
+   
+    @IBAction func uploadPhotoButton(sender: AnyObject) {
+        
+        var imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]!) {
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+        println("User picked an image")
+        
+        var editedImage = info[UIImagePickerControllerOriginalImage] as UIImage
+        
+        self.imageView.image = editedImage
+        self.peopleDestination!.image = editedImage
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController!) {
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
     }
     
     
